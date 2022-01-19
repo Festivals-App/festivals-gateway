84b8e1b6dc136f5f11a5e6a47277335f74f7df05
Simon Gaus <phisto05@gmail.com>
2022-01-19 11:38:10 +0100

    Refactor loadbalancing

--- server/handler/heartbeat.go
+++ server/handler/heartbeat.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/Festivals-App/festivals-gateway/server/config"
@@ -12,18 +15,27 @@ import (
 )
 
 type Service struct {
-	Name  string
-	URL   string
-	At    time.Time
-	Alive bool
+	Name         string
+	URL          *url.URL
+	At           time.Time
+	Alive        bool
+	mux          sync.RWMutex
+	ReverseProxy *httputil.ReverseProxy
+}
+
+func (s *Service) IsAlive() (alive bool) {
+	s.mux.RLock()
+	alive = s.Alive
+	s.mux.RUnlock()
+	return
 }
 
 type ServicePool struct {
-	Services []Service
+	Services []*Service
 	Current  uint64
 }
 
-var ServicePools map[string]ServicePool = map[string]ServicePool{}
+var ServicePools map[string]*ServicePool = map[string]*ServicePool{}
 
 func ReceivedHeartbeat(conf *config.Config, w http.ResponseWriter, r *http.Request) {
 
@@ -34,24 +46,30 @@ func ReceivedHeartbeat(conf *config.Config, w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	service := Service{Name: beat.Service, URL: "http://" + beat.Host + ":" + strconv.Itoa(beat.Port), At: time.Now(), Alive: beat.Available}
+	url, err := url.Parse("http://" + beat.Host + ":" + strconv.Itoa(beat.Port))
+	if err != nil {
+		respondError(w, http.StatusBadRequest, "Failed to read heartbeat URL!")
+		return
+	}
+
+	service := &Service{Name: beat.Service, URL: url, At: time.Now(), Alive: beat.Available}
 	registerService(service)
 	respondCode(w, http.StatusAccepted)
 }
 
-func registerService(service Service) {
+func registerService(service *Service) {
 
 	servicePool, exists := ServicePools[service.Name]
 	if !exists {
 		fmt.Printf("Initially created service pool for: %+v\n", service.Name)
-		ServicePools[service.Name] = ServicePool{Services: []Service{}, Current: 0}
+		ServicePools[service.Name] = &ServicePool{Services: []*Service{}, Current: 0}
 		registerService(service)
 	} else {
 
 		known, index := knownInstance(service, servicePool.Services)
 		if known {
-			servicePool.Services[index] = service
-			fmt.Printf("Replaced instance: %+v\n", service)
+			servicePool.Services[index].At = time.Now()
+			fmt.Printf("Updated instance: %+v\n", service)
 
 		} else {
 			fmt.Printf("Initially added: %+v\n", service)
@@ -62,9 +80,14 @@ func registerService(service Service) {
 	}
 }
 
-func knownInstance(service Service, list []Service) (bool, int) {
+func knownInstance(service *Service, list []*Service) (bool, int) {
+
 	for i, member := range list {
-		if member.URL == service.URL {
+
+		a := member.URL
+		b := service.URL
+
+		if *a == *b {
 			return true, i
 		}
 	}
--- server/handler/loadbalance.go
+++ server/handler/loadbalance.go
@@ -1,44 +1,72 @@
 package handler
 
 import (
-	"math/rand"
+	"errors"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"time"
+	"sync/atomic"
 
 	"github.com/Festivals-App/festivals-gateway/server/config"
 )
 
 func GoToFestivalsAPI(conf *config.Config, w http.ResponseWriter, r *http.Request) {
 
-	pool, exists := ServicePools["festivals-server"]
-	if !exists {
-		respondError(w, http.StatusServiceUnavailable, "No available backend server")
-		return
-	}
-
-	services := pool.Services
-
-	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
-	service := services[rand.Intn(len(services))]
-
-	url, err := url.Parse(service.URL)
+	url, err := loadbalancedHost("festivals-server")
 	if err != nil {
-		respondError(w, http.StatusServiceUnavailable, "Backend server address not reasonable.")
+		respondError(w, http.StatusServiceUnavailable, err.Error())
 		return
 	}
 
 	rp := httputil.NewSingleHostReverseProxy(url)
 	rp.ServeHTTP(w, r)
-
 }
 
 func GoToFestivalsFilesAPI(conf *config.Config, w http.ResponseWriter, r *http.Request) {
 
-	//respondString(w, http.StatusOK, "FESTIVALS_FILES_API")
+	url, err := loadbalancedHost("festivals-fileserver")
+	if err != nil {
+		respondError(w, http.StatusServiceUnavailable, err.Error())
+		return
+	}
 
-	u, _ := url.Parse("http://localhost:1910")
-	rp := httputil.NewSingleHostReverseProxy(u)
+	rp := httputil.NewSingleHostReverseProxy(url)
 	rp.ServeHTTP(w, r)
+}
+
+func loadbalancedHost(serviceIdentifier string) (*url.URL, error) {
+
+	pool, exists := ServicePools[serviceIdentifier]
+	if !exists {
+		return nil, errors.New("loadbalancer: no available backend server for " + serviceIdentifier)
+	}
+
+	peer := pool.GetNextPeer()
+	if peer != nil {
+		return peer.URL, nil
+	}
+
+	return nil, errors.New("loadbalancer: no available backend server for " + serviceIdentifier)
+}
+
+func (s *ServicePool) NextIndex() int {
+	return int(atomic.AddUint64(&s.Current, uint64(1)) % uint64(len(s.Services)))
+}
+
+// GetNextPeer returns next active peer to take a connection
+func (s *ServicePool) GetNextPeer() *Service {
+	// loop entire backends to find out an Alive backend
+	next := s.NextIndex()
+	l := len(s.Services) + next // start from next and move a full cycle
+	for i := next; i < l; i++ {
+		idx := i % len(s.Services) // take an index by modding with length
+		// if we have an alive backend, use it and store if its not the original one
+		if s.Services[idx].IsAlive() {
+			if i != next {
+				atomic.StoreUint64(&s.Current, uint64(idx)) // mark the current one
+			}
+			return s.Services[idx]
+		}
+	}
+	return nil
 }
