3c8ac371cdb8ef3ba19a11d95eeddb86e7c6c5ad
Simon Gaus <phisto05@gmail.com>
2022-03-03 04:45:41 +0100

    Update template

--- main.go
+++ main.go
@@ -6,15 +6,30 @@ import (
 
 	"github.com/Festivals-App/festivals-gateway/server"
 	"github.com/Festivals-App/festivals-gateway/server/config"
+	"github.com/Festivals-App/festivals-gateway/server/logger"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
+
+	logger.Initialize("/var/log/festivals-gateway/info.log")
+
+	log.Info().Msg("Server startup.")
+
 	conf := config.DefaultConfig()
 	if len(os.Args) > 1 {
 		conf = config.ParseConfig(os.Args[1])
 	}
 
+	log.Info().Msg("Server configuration was initialized.")
+
 	serverInstance := &server.Server{}
 	serverInstance.Initialize(conf)
-	serverInstance.Run(conf.ServiceBindAddress + ":" + strconv.Itoa(conf.ServicePort))
+
+	go serverInstance.Run(conf.ServiceBindAddress + ":" + strconv.Itoa(conf.ServicePort))
+	log.Info().Msg("Server did start.")
+
+	// wait forever
+	// https://stackoverflow.com/questions/36419054/go-projects-main-goroutine-sleep-forever
+	select {}
 }
