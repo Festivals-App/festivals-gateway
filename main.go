8bc151c7440f8d9bdcc4ecae566f45036479a0d7
Simon Gaus <phisto05@gmail.com>
2022-03-21 02:48:16 +0100

    Use host to bind to

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	serverInstance := &server.Server{}
 	serverInstance.Initialize(conf)
 
-	go serverInstance.Run(conf.ServiceBindAddress + ":" + strconv.Itoa(conf.ServicePort))
+	go serverInstance.Run(conf.ServiceBindHost + ":" + strconv.Itoa(conf.ServicePort))
 	log.Info().Msg("Server did start.")
 
 	go sendHeartbeat(conf)
@@ -43,7 +43,7 @@ func sendHeartbeat(conf *config.Config) {
 	for {
 		timer := time.After(time.Second * 2)
 		<-timer
-		var beat *heartbeat.Heartbeat = &heartbeat.Heartbeat{Service: "festivals-gateway", Host: conf.ServiceBindAddress, Port: conf.ServicePort, Available: true}
+		var beat *heartbeat.Heartbeat = &heartbeat.Heartbeat{Service: "festivals-gateway", Host: conf.ServiceBindHost, Port: conf.ServicePort, Available: true}
 		err := heartbeat.SendHeartbeat(conf.LoversEar, conf.ServiceKey, beat)
 		if err != nil {
 			log.Error().Err(err).Msg("Failed to send heartbeat")
